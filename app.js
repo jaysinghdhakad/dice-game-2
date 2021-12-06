efe16bb51f7117cd7935b41bbf08fde572706ee9
jaysinghdhakad <jaysingh20196@gmail.com>
2021-12-06 16:09:21 +0530

    made changes according to review

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const diceOutput = document.querySelector(".output");
 const restartButton = document.querySelector(".restart");
 setDisplayOnDom("none", restartButton);
 setDisplayOnDom("none", rollDice);
+
 let cutoffScore;
 let toss;
 let turn;
@@ -23,49 +24,58 @@ function getCutoffScore() {
   setInitialScore();
   setDisplayOnDom("block", rollDice);
 }
+
 function setInitialScore() {
   toss = Math.round(Math.random());
   setValueOnDom(0, player1Score);
   setValueOnDom(0, player2Score);
   setTurn(toss);
 }
+
 function setTurn(value) {
   turn = value;
   turn === 0
     ? setValueOnDom("player one's turn ", turnIndicator)
     : setValueOnDom("player two's turn ", turnIndicator);
 }
+
 function rollthedice() {
-  const output = Math.round(Math.random() * 5 + 1);
-  setValueOnDom(output, diceOutput);
-  addTheScore(output);
+  const rollDiceOutCome = generateRnadomNum1to6();
+  setValueOnDom(rollDiceOutCome, diceOutput);
+  addTheScore(rollDiceOutCome);
   checkScore();
-  output !== 6 ? (turn = turn === 0 ? 1 : 0) : turn;
+  rollDiceOutCome !== 6 ? (turn = turn === 0 ? 1 : 0) : turn;
   setTurn(turn);
 }
+
 function checkScore() {
   scorePlayer1 >= cutoffScore ? declareWinner(1) : null;
   scorePlayer2 >= cutoffScore ? declareWinner(2) : null;
 }
+
 function declareWinner(value) {
   value === 1
     ? setValueOnDom("player one is the  winner", diceOutput)
     : setValueOnDom("player two is the winner ", diceOutput);
   stoptheGame();
 }
+
 function addTheScore(value) {
-  turn === 0
-    ? (scorePlayer1 = scorePlayer1 + value)
-    : (scorePlayer2 = scorePlayer2 + value);
-  turn === 0
-    ? setValueOnDom(scorePlayer1, player1Score)
-    : setValueOnDom(scorePlayer2, player2Score);
+  if (turn === 0) {
+    scorePlayer1 = scorePlayer1 + value;
+    setValueOnDom(scorePlayer1, player1Score);
+  } else {
+    scorePlayer2 = scorePlayer2 + value;
+    setValueOnDom(scorePlayer2, player2Score);
+  }
 }
+
 function stoptheGame() {
   setDisplayOnDom("none", rollDice);
   setDisplayOnDom("none", turnIndicator);
   setDisplayOnDom("block", restartButton);
 }
+
 function restartGame() {
   setDisplayOnDom("block", turnIndicator);
   setDisplayOnDom("block", cutoffSubmitButton);
@@ -80,12 +90,20 @@ function restartGame() {
   scorePlayer1 = 0;
   scorePlayer2 = 0;
 }
+
 function setDisplayOnDom(value, element) {
   element.style.display = value;
 }
+
 function setValueOnDom(value, element) {
   element.innerHTML = value;
 }
+
+function generateRnadomNum1to6() {
+  const outPut = Math.round(Math.random() * 5 + 1);
+  return outPut;
+}
+
 restartButton.addEventListener("click", restartGame);
 rollDice.addEventListener("click", rollthedice);
 cutoffSubmitButton.addEventListener("click", getCutoffScore);
